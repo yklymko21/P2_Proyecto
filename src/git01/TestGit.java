bf3f1c711471a0c0d33202e3a41c8a648ca5484b
Yaroslav Klymko <yklymko21@ilg.cat>
2022-11-07 20:22:05 +0100

    Cuarto Commit

--- src/git01/TestGit.java
+++ src/git01/TestGit.java
@@ -1,10 +1,14 @@
 package git01;
 
+import java.util.Scanner;
+
 public class TestGit {
 
 	public static void main(String[] args) {
-		String name = "Bugs Bunny";
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
 		System.out.println("Hello " + name + "!");
+		scanner.close();
 	}
 
 }
