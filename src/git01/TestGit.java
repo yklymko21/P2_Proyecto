5ee1e6c96c4d92894e9178834a84b9131b7fd332
Yaroslav Klymko <yklymko21@ilg.cat>
2022-11-07 20:24:10 +0100

    Quinto Commit

--- src/git01/TestGit.java
+++ src/git01/TestGit.java
@@ -4,7 +4,6 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		String name = "Bugs Bunny";
-		System.out.println("Hello " + name.toUpperCase()  + "!");
+		System.out.println("Hello Mr." + name.toUpperCase()  + "!");
 	}
-
 }
