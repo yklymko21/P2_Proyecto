41850de050d4c50c59733716091d5f3dbb52becf
Yaroslav Klymko <yklymko21@ilg.cat>
2022-11-07 17:46:26 +0100

    Tercer Commit

--- src/git01/TestGit.java
+++ src/git01/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		String name = "Bugs Bunny";
-		System.out.println("Hello " + name + "!");
+		System.out.println("Hello " + name.toUpperCase()  + "!");
 	}
 
 }
